3119b9d30a1a8c0ef8141bbc7812e5291cc22651
stevenojob <achimvp1@gmail.com>
2023-11-29 14:55:15 +0100

    last push!!!!!

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
     
     sheet(input_word="PASSWORD-MANAGER", input_width=40) #create template
     crypter = key_manager() #create or load an existing key.
+
     while True:
         if sign_in(crypter):
             pw_manager: PWManager = PWManager(crypter=crypter)  
             pw_manager.main_menu()
         else: 
             break
-
 
 if __name__ == "__main__":
     try: 
